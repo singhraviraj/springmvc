4b23cbd672f9522f514f17cae2e256685c9084e9
raviraj <singhraviraj94@gmail.com>
2024-07-08 11:02:25 +0530

    1.1 springmvc setting up

---
+++ src/main/java/com/springBootmvc/springBootmvc/SpringBootmvcApplication.java
@@ -0,0 +1,13 @@
+package com.springBootmvc.springBootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBootmvc/springBootmvc/SpringBootmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootmvc.springBootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
